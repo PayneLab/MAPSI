3788ccc680e74249a930f85624dde47b5142ffca
Sarah Curtis <105232453+sarah2233@users.noreply.github.com>
2022-05-20 13:13:25 -0600

    update parsers

---
+++ test_parser_msfragger.py
@@ -0,0 +1,10 @@
+import unittest
+from MSFragger_Parser import *
+
+# mzML file
+mzml_file_path = "C:\\Users\\Sarah Curtis\\OneDrive - BYU\\Documents\\Single Cell Team Documents\\API_dev\\MetaM\\2ng\\Ex_Auto_J3_30umTB_2ngQC_60m_1.mzML"
+
+# MSFragger files
+msfragger_psm_file_path = "C:\\Users\\Sarah Curtis\\OneDrive - BYU\\Documents\\Single Cell Team Documents\\API_dev\\msfragger\\psm1.tsv"
+msfragger_peptide_file_path = "C:\\Users\\Sarah Curtis\\OneDrive - BYU\\Documents\\Single Cell Team Documents\\API_dev\\msfragger\\combined_peptide.tsv"
+msfragger_protein_file_path = "C:\\Users\\Sarah Curtis\\OneDrive - BYU\\Documents\\Single Cell Team Documents\\API_dev\\msfragger\\combined_protein.tsv"
